fb6770c3457597ca56359418984264398e2bddd1
Laura Minaya <minayalaurac@gmail.com>
2021-10-21 17:24:10 -0400

    harrypottergame

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-let userName = window.prompt('Please enter your name:')
-let userScore = 0;
+let firstAnswer  = window.prompt('Welcome to Hogwarts where you have been sorted into Slytherin House! Upon exploring the grounds you come across a path that forks two ways. Do you go LEFT or RIGHT?');
 
-for(let i = 0; i < questions.length; i++){
-    let question = questions[i]
-    let userAnswer = window.prompt(question.text)
-    if(userAnswer === question.correctAnswer){
-        userScore = userScore + 10
+console.log(firstAnswer);
+
+if(firstAnswer === 'left') {
+    console.log("You entered left!");
+
+    let secondAnswer = window.prompt(`You are heading to the forbidden forest! Go THROUGH or AROUND?`);
+    console.log(secondAnswer);
+
+    if (secondAnswer === 'through') {
+        console.log('You wenth through the forest!');
+
+        let thirdAnswer = window.prompt('You entered the forbidden forest and you are approached by the Dark Lord Voldemort! He asks you to become a death eater and join the Dark Army. Do you ACCEPT or REFUSE his invitation?');
+        console.log('thirdAnswer');
+
+    } else if (secondAnswer === 'around') {
+        console.log('You went around!');
+
+        let thirdAnswer = window.prompt('You went around the forest and ran into Bellatrix Lestrange! Bellatrix, the infamous death eater, uses the crucio curse on you and forces you to join the Dark Army.. do you work as a DOUBLE AGENT and tell the good guys all the Dark Armys plans? Or do you faithfully SERVE the Dark Army?');
+        console.log('thirdAnswer');
     }
-}
 
-window.alert('Your score is: '+userScore)
 
-let serializedGames = localStorage.getItem('games')
 
-let games;
-if(serializedGames === null){
-    games = [] 
+} else if(firstAnswer === 'right') {
+    console.log("You entered right!");
+
+    let secondAnswer = window.prompt(`You are heading to the Whomping Willow! A sentient and violent magical tree that attacks everything in its way! Do you try to go AROUND or go UNDER into a small tunnel?`);
+    console.log(secondAnswer);
+
+    if (secondAnswer === 'under') {
+        console.log('You went under the whomping willow into the tunnel!');
+
+        let thirdAnswer = window.prompt('You went through the tunnel under the whomping willow and it leads you inside the Shrieking Shack. ');
+        console.log('thirdAnswer');
+
+    } else if (secondAnswer === 'around') {
+        console.log('You went around!');
+
+        let thirdAnswer = window.prompt('You went around the whomping willow and realize that it is dark and a full moon tonight. Before you hear it, a werewolf jumps in front of you! Do you use STUPEFY, the stunning spell to render the werewolf temporarily unconscious? or AVADA KEDAVRA, the killing curse?');
+        console.log('thirdAnswer');
+    }
+
+
 } else {
-    games = JSON.parse(serializedGames)
+    console.log('You did not enter a valid response..to the Dungeon!')
 }
-
-let game = {
-    user: userName,
-    score: userScore
-}
-
-games.push(game)
-
-serializedGames = JSON.stringify(games)
-localStorage.setItem('games', serializedGames)
-
-let highestScoringGame = games[0]
-for(let i = 0; i < games.length; i++){
-    let game = games[i]
-    if(game.score > highestScoringGame.score){
-        highestScoringGame = game
-    }
-}
-window.alert('The high score is: '+highestScoringGame.score)
